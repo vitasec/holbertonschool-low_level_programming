cf71dc95f6daa8cacceaa20da179d50957ff29d3
C1D4v1d <davidabdurahmanov38@gmail.com>
2025-11-27 13:49:33 +0000

    not grandeur, and territor

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 
 /**
- * main - entry point of the program
- * Return: always 0
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+    printf("Size of a char: %lu byte(s)\n", sizeof(char));
+    printf("Size of an int: %lu byte(s)\n", sizeof(int));
+    printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+    printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+    printf("Size of a float: %lu byte(s)\n", sizeof(float));
+    
+    return (0);
 }
-
