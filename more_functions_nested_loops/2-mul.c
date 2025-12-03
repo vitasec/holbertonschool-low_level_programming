d38655d693ab345ca132ce8f1b774a233c296bce
psevdoner <davidabdurahmanov38@gmail.com>
2025-12-03 00:23:43 +0000

    task 2 multi a*b

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ */
+
+int mul(int a, int b)
+{
+	return  (a * b);
+}
+
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -3,5 +3,10 @@
 
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int a, int b);
+
+
+
+
 #endif
 
