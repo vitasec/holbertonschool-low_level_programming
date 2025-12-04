fe3f814c9b2b08351efd6d326b06b02676747644
vitasec <davidabdurahmanov38@gmail.com>
2025-12-04 08:27:27 +0000

    100 times

---
+++ functions_nested_loops/100-times_table.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints all natural numbers from n to 98
+ * @n: starting number
+ *
+ * Return: void
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (; n <= 98; n++)
+		{
+			printf("%d", n);
+			if (n != 98)
+				printf(", ");
+		}
+	}
+	else
+	{
+		for (; n >= 98; n--)
+		{
+			printf("%d", n);
+			if (n != 98)
+				printf(", ");
+		}
+	}
+	printf("\n")
+
