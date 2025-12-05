1408c76c767f86d4a194ce9b75ec989015602df9
vitasec <davidabdurahmanov38@gmail.com>
2025-12-05 15:52:25 +0000

    101 is tame table

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -13,6 +13,8 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
+void print_int(int n);
+int _putchar(char c);
 
 // 🛠️ DƏYİŞİKLİK 1: print_times_table sənədləşməsi
 /**
