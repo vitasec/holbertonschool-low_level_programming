421fc661f30d06b427cfcc4721dc5a2f3047bbca
vitasec <davidabdurahmanov38@gmail.com>
2025-12-05 16:06:09 +0000

    101 time table

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -14,9 +14,9 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_int(int n);
-int _putchar(char c);
+/* int _putchar(char c); - Bu təkrar olunduğu üçün silinə bilər, amma yuxarıdakı şərhi istifadə edək. */
 
-// 🛠️ DƏYİŞİKLİK 1: print_times_table sənədləşməsi
+/* 🛠️ DƏYİŞİKLİK 1: print_times_table sənədləşməsi */
 /**
  * print_times_table - prints the n times table, starting with 0
  * @n: the number to generate the table for
@@ -25,9 +25,9 @@ int _putchar(char c);
  */
 void print_times_table(int n);
 
-// _putchar təkrar olunur, onu silmək olar, amma betty üçün əsas deyil.
+/* _putchar təkrar olunur, onu silmək olar, amma betty üçün əsas deyil. */
 
-// 🛠️ DƏYİŞİKLİK 2: print_number sənədləşməsi
+/* 🛠️ DƏYİŞİKLİK 2: print_number sənədləşməsi */
 /**
  * print_number - prints an integer using only _putchar
  * @n: number to print
