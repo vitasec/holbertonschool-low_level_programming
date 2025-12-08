37d16bd46d5184df6a4bc43d460f4e01b3a95145
vitasec <davidabdurahmanov38@gmail.com>
2025-12-08 08:44:44 +0000

    Task 0

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Updates the value pointed to by n to 98
+ * @n: Pointer to an integer
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
