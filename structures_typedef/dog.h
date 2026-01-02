eac9c96b2c445fbf5816e95fa9a9ca2f3c26110b
vitasec <davidabdurahmnaov38@gmail.com>
2026-01-02 14:48:50 +0000

    First task

---
+++ structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+* struct dog-melumatlarin saxlayir
+*@name: adi
+*@age: yasi
+*@owner: sahibi
+*
+*Description: itinin adi,yasi ve sahibi ozunde saxlayan struktur
+*/
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+}
+
+#endif
